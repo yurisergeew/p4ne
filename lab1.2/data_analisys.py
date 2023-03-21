473d59933b47cfc32d7c0f008bc6c15effc14687
Yuri Sergeev <sergeev@jet.su>
2023-03-21 18:33:41 +0300

    first commit

--- lab1.2/data_analisys.py
+++ lab1.2/data_analisys.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot
 from openpyxl import load_workbook
 
-wb=load_workbook('data_analysis_lab.xls')
+wb=load_workbook('data_analysis_lab.xlsx')
 sheet = wb['Data']
 sheet['A'][1:]
 
